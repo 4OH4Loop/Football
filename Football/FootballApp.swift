809f44f73ac6d6d86ed8b1bc8ccc830ac413d470
4OH4Loop <semperchuck@gmail.com>
2025-01-19 15:17:29 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Carolyn Ballinger on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
